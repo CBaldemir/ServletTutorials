a62259b4710a0199aa6ddb6f1ab3a41f0d66dde7
CBaldemir <comertbaldemir@gmail.com>
2017-06-11 16:58:56 +0300

    Servlet Giris

---
+++ src/HelloServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+
+/**
+ * Created by comer on 11.06.2017.
+ */
+public class HelloServlet extends HttpServlet{
+    @Override
+    public  void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        PrintWriter printWriter=response.getWriter();
+        String message="Hello Servlet! Comert Baldemirr";
+        printWriter.write(message);
+
+    }
+
+}
